c2631fa0e5f5065e283cb90e493696bb513d41ea
UdayKiran100 <uday@labh.io>
2026-06-02 10:52:21 +0530

    02-06-2026

---
+++ Arrays_level_2/3_sum.py
@@ -0,0 +1,49 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+"""
+class Solution:
+    def threeSum(self, nums)
+        nums.sort()
+        n = len(nums)
+        result = []
+        for i in range(n-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            target = -nums[i]
+            left = i + 1
+            right = n - 1
+            while left<right:
+                c_sum = nums[left]+nums[right]
+                if c_sum==target:
+                    result.append([nums[i],nums[left],nums[right]])
+                    left+=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1
+                    right-=1
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                else:
+                    if c_sum<target:
+                        left+=1
+                    else:
+                        right-=1
+        return result
+
+# Example usage:
+solution = Solution()
+nums = [-1, 0, 1, 2, -1, -4]
+print(solution.threeSum(nums))  # Output: [[-1, -1, 2], [-1, 0, 1]] 
