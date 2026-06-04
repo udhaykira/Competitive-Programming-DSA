ea6caaa3f4517f6dc9d1c5a9cfc70ea44840e859
udhaykira <uday@labh.io>
2026-06-04 20:46:58 +0530

    Arrays level 3

---
+++ Arrays_level_3/product_except_itself.py
@@ -0,0 +1,37 @@
+"""
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+"""
+
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        prefix = [1]*n
+        suffix = [1]*n
+        for i in range(1,n):
+            prefix[i] = prefix[i-1]*nums[i-1]
+        for i in range(n-2,-1,-1):
+            suffix[i] = suffix[i+1]*nums[i+1]
+        result = [0]*n
+        for i in range(n):
+            result[i] = prefix[i]*suffix[i]
+        return result
+
+# Example usage:
+solution = Solution()
+nums = [1,2,3,4]
+print(solution.productExceptSelf(nums))  # Output: [24,12,8,6]
