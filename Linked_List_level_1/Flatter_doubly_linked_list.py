290c1db4209f7d5ea7f9c73631b0ea08ea0a70f8
udhaykira <2003udhaykiran@gmail.com>
2026-06-07 18:05:40 +0530

    Linked List Problems

---
+++ Linked_List_level_1/Flatter_doubly_linked_list.py
@@ -0,0 +1,48 @@
+"""
+LeetCode : 430
+Flatten a Multilevel Doubly Linked List
+
+You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer, and an additional child pointer. This child pointer may or may not point to a separate doubly linked list, also containing these special nodes. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure as shown in the example below.
+
+Given the head of the first level of the list, flatten the list so that all the nodes appear in a single-level, doubly linked list. Let curr be a node with a child list. The nodes in the child list should appear after curr and before curr.next in the flattened list.
+
+Return the head of the flattened list. The nodes in the list must have all of their child pointers set to null.
+
+Example 1 : 
+
+Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+
+"""
+
+
+# Definition for a Node.
+# class Node:
+#     def __init__(self, val, prev, next, child):
+#         self.val = val
+#         self.prev = prev
+#         self.next = next
+#         self.child = child
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+        curr = head
+        stack = []
+        while curr:
+            if curr.child is None:
+                if curr.next is None and stack!=[]:
+                    last_node = stack.pop()
+                    curr.next = last_node
+                    last_node.prev = curr
+                curr = curr.next
+            else:
+                if curr.next:
+                    stack.append(curr.next)
+                curr.next = curr.child
+                curr.child.prev = curr
+                curr.child = None
+                curr = curr.next
+        return head
+        
---
+++ Linked_List_level_1/merge_two_sorted_linked_list.py
@@ -0,0 +1,48 @@
+"""
+LeetCode : 21
+Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        tail = head
+        curr1 = list1
+        curr2 = list2
+        while curr1 and curr2:
+            if curr1.val<=curr2.val:
+                tail.next = curr1
+                curr1 = curr1.next
+            else:
+                tail.next = curr2
+                curr2 = curr2.next
+            tail = tail.next
+        
+        if curr1:
+            tail.next = curr1
+        else:
+            tail.next = curr2
+        return head.next
